385ca555978ee5d913513efb2f13e579d3df5f13
Jordy Manner <jordy.manner@milkcreation.fr>
2018-03-28 10:41:04 +0000

    shop -- branch 1.0 -- Traitement du paiement en ligne avant paymentComplete

--- Functions/UrlInterface.php
+++ Functions/UrlInterface.php
@@ -5,30 +5,36 @@ namespace tiFy\Plugins\Shop\Functions;
 interface UrlInterface
 {
     /**
-     * Url vers une page de la boutique
-     *
+     * Url vers une page de la boutique.
      * @param string $name Nom de la page. shop|cart|checkout|terms.
-     *
      * @return string
      */
     public function page($name);
+
     /**
-     * Url vers la page d'accueil de la boutique
+     * Url vers la page d'accueil de la boutique.
      * @return string
      */
     public function shopPage();
 
     /**
-     * Url vers la page d'affichage du panier
+     * Url vers la page d'affichage du panier.
      * @return string
      */
     public function cartPage();
 
     /**
-     * Url vers la page de commande
+     * Url vers la page de commande.
      * @return string
      */
     public function checkoutPage();
+
+    /**
+     * Url vers la page de demande de paiement de la commande.
+     * @param array $args Liste des arguments de requête
+     * @return string
+     */
+    public function checkoutOrderPayPage($args = []);
 
     /**
      * Url vers la page de commande reçue.
@@ -45,20 +51,18 @@ interface UrlInterface
 
     /**
      * Url vers la page de suppression de moyen de paiement.
-     *
      * @return string
      */
     public function checkoutDeletePaymentMethodPage();
 
     /**
      * Url vers la page de définition du moyen de paiement par défaut.
-     *
      * @return string
      */
     public function checkoutSetDefaultPaymentMethodPage();
 
     /**
-     * Url vers la page des conditions générales de vente
+     * Url vers la page des conditions générales de vente.
      * @return string
      */
     public function termsPage();

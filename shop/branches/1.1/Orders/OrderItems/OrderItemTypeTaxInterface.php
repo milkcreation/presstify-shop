7e0d4e7e4285e123119d1cfecc97db1bddf99675
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-04-26 12:47:28 +0000

    shop -- branch 1.1 -- création

---
+++ shop/branches/1.1/Orders/OrderItems/OrderItemTypeTaxInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace tiFy\Plugins\Shop\Orders\OrderItems;
+
+interface OrderItemTypeTaxInterface extends OrderItemTypeInterface
+{
+
+}
---
+++ shop/branches/1.1/Products/ProductItemInterface.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace tiFy\Plugins\Shop\Products;
+
+use tiFy\Core\Query\Controller\PostItemInterface;
+
+interface ProductItemInterface extends PostItemInterface
+{
+    /**
+     * Récupération de la liste des types de produit.
+     *
+     * @return ObjectTypes\Categorized|ObjectTypes\Uncategorized
+     */
+    public function getProductObjectType();
+
+    /**
+     * Récupération de la liste des types de produit.
+     *
+     * @return array
+     */
+    public function getProductTypes();
+
+    /**
+     * Récupération du type de produit.
+     *
+     * @return string
+     */
+    public function getProductType();
+
+    /**
+     * Récupération de l'Unité de Gestion de Stock (SKU).
+     *
+     * @return string
+     */
+    public function getSku();
+
+    /**
+     * Récupération du prix de vente.
+     *
+     * @return float
+     */
+    public function getRegularPrice();
+
+    /**
+     * Récupération du prix de vente affiché.
+     *
+     * @return string
+     */
+    public function salePriceDisplay();
+
+    /**
+     * Récupération du poids.
+     *
+     * @return float
+     */
+    public function getWeight();
+
+    /**
+     * Récupération des attributs.
+     *
+     * @return array
+     */
+    public function getAttributes();
+
+    /**
+     * Récupération des attribut option d'achat.
+     *
+     * @param string $name Identifiant de qualification de l'option d'achat.
+     *
+     * @return ProductPurchasingOption
+     */
+    public function getPurchasingOption($name);
+
+    /**
+     * Récupération des produits du groupe.
+     *
+     * @return string[]
+     */
+    public function getGroupedProducts();
+
+    /**
+     * Récupération des produits de montée en gamme.
+     *
+     * @return string[]
+     */
+    public function getUpsellProducts();
+
+    /**
+     * Récupération du la liste des étiquettes associées.
+     *
+     * @return array|\WP_Term[]
+     */
+    public function getProductTags();
+
+    /**
+     * Vérifie si un produit est téléchargeable.
+     *
+     * @return bool
+     */
+    public function isDownloadable();
+
+    /**
+     * Vérifie si un produit est dématérialisé (virtuel).
+     *
+     * @return bool
+     */
+    public function isVirtual();
+
+    /**
+     * Vérifie si un produit est mis en avant.
+     *
+     * @return bool
+     */
+    public function isFeatured();
+
+    /**
+     * Vérifie si un produit est en droit d'être commandé.
+     *
+     * @return bool
+     */
+    public function isPurchasable();
+
+    /**
+     * Vérifie si un produit est en stock.
+     *
+     * @return bool
+     */
+    public function isInStock();
+
+    /**
+     * Sauvegarde des données d'un produit.
+     *
+     * @return void
+     */
+    public function save();
+}

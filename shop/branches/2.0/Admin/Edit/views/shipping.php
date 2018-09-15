a817340658a8c43d0607bc5e236fab5b0182695f
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-09-15 09:20:05 +0000

    shop -- branch 2.0 -- Vue d'édition interface admin

--- shop/branches/2.0/Admin/Edit/views/shipping.php
+++ shop/branches/2.0/Admin/Edit/views/shipping.php
@@ -7,64 +7,70 @@
 
 <table class="form-table">
     <tbody>
-        <tr>
-            <th>
+    <tr>
+        <th>
             <?php
-                echo tify_field_label(
-                    [
-                        'content' => __('Poids (kg)', 'tify')
-                    ]
-                );
+            echo field(
+                'label',
+                [
+                    'content' => __('Poids (kg)', 'tify'),
+                ]
+            );
             ?>
-            </th>
-            <td>
+        </th>
+        <td>
             <?php
-                echo tify_field_text(
-                    [
-                        'name' => '_weight',
-                        'value' => get_post_meta($post->ID, '_weight', true)
-                    ]
-                );
+            echo field(
+                'text',
+                [
+                    'name'  => '_weight',
+                    'value' => get_post_meta($post->ID, '_weight', true),
+                ]
+            );
             ?>
-            </td>
-        </tr>
-        <tr>
-            <th>
+        </td>
+    </tr>
+    <tr>
+        <th>
             <?php
-                echo tify_field_label(
-                    [
-                        'content' => __('Dimensions (cm)', 'tify')
-                    ]
-                );
+            echo field(
+                'label',
+                [
+                    'content' => __('Dimensions (cm)', 'tify'),
+                ]
+            );
             ?>
-            </th>
-            <td>
+        </th>
+        <td>
             <?php
-                echo tify_field_text(
-                    [
-                        'name' => '_length',
-                        'value' => get_post_meta($post->ID, '_length', true)
-                    ]
-                );
-            ?>
-            <?php
-                echo tify_field_text(
-                    [
-                        'name' => '_width',
-                        'value' => get_post_meta($post->ID, '_width', true)
-                    ],
-                    true
-                );
+            echo field(
+                'text',
+                [
+                    'name'  => '_length',
+                    'value' => get_post_meta($post->ID, '_length', true),
+                ]
+            );
             ?>
             <?php
-                echo tify_field_text(
-                    [
-                        'name' => '_height',
-                        'value' => get_post_meta($post->ID, '_height', true)
-                    ]
-                );
+            echo field(
+                'text',
+                [
+                    'name'  => '_width',
+                    'value' => get_post_meta($post->ID, '_width', true),
+                ],
+                true
+            );
             ?>
-            </td>
-        </tr>
+            <?php
+            echo field(
+                'text',
+                [
+                    'name'  => '_height',
+                    'value' => get_post_meta($post->ID, '_height', true),
+                ]
+            );
+            ?>
+        </td>
+    </tr>
     </tbody>
 </table>

8bb2f14939369d7390a7d520b04defe30d2b456a
Jordy Manner <jordy.manner@milkcreation.fr>
2018-10-12 08:27:05 +0000

    shop -- branch 2.0 -- correctif mineur

--- Shop.php
+++ Shop.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/shop
  * @namespace \tiFy\Plugins\Shop
- * @version 2.0.0
+ * @version 2.0.1
  */
 
 namespace tiFy\Plugins\Shop;

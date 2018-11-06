033e8ebe3984418ac8239b378ab22fefcc8b3385
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-11-06 08:42:51 +0000

    shop -- tag 2.0.9 -- Dépendances interface + purchasing_options

---
+++ shop/tags/2.0.9/Addresses/Shipping.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @name Shipping
+ * @desc Gestion des adresses de livraison.
+ *
+ * @author Jordy Manner <jordy@tigreblanc.fr>
+ * @copyright Milkcreation
+ */
+
+namespace tiFy\Plugins\Shop\Addresses;
+
+use tiFy\Plugins\Shop\Contracts\AddressShippingInterface;
+
+class Shipping extends AbstractAddress implements AddressShippingInterface
+{
+
+}

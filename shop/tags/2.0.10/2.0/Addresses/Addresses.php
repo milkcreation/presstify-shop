c9103d15130b3e79e98507eaba3119b3ab20fd92
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-02-23 09:54:08 +0000

    shop -- tag 2.0.10 -- FormHandler : Correctif - Adaptation code globale

---
+++ shop/tags/2.0.10/2.0/Addresses/Addresses.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace tiFy\Plugins\Shop\Addresses;
+
+use tiFy\Plugins\Shop\AbstractShopSingleton;
+use tiFy\Plugins\Shop\Contracts\AddressesInterface;
+
+/**
+ * Class Addresses
+ *
+ * @desc Gestion des adresses : livraison|facturation|pays
+ */
+class Addresses extends AbstractShopSingleton implements AddressesInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function boot()
+    {
+        form()->addonRegister('shop.addresses.form_handler', function($name, $attrs, $form) {
+            return app('shop.addresses.form_handler', [$name, $attrs, $form, $this->shop]);
+        });
+
+        $this->billing();
+        $this->shipping();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function billing()
+    {
+        return app('shop.addresses.billing', [$this, $this->shop]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function defaultFields()
+    {
+        return [
+            'first_name' => [
+                'title'        => __('Prénom', 'tify'),
+                'type'         => 'text',
+                'required'     => true,
+                'attrs'        => [
+                    'autocomplete' => 'given-name',
+                ],
+                'order'        => 10,
+            ],
+            'last_name'  => [
+                'title'        => __('Nom de famille', 'tify'),
+                'type'         => 'text',
+                'required'     => true,
+                'attrs'        => [
+                    'autocomplete' => 'family-name',
+                ],
+                'order'        => 20,
+            ],
+            'company'    => [
+                'title'        => __('Société', 'tify'),
+                'type'         => 'text',
+                'attrs'        => [
+                    'autocomplete' => 'organization',
+                ],
+                'order'        => 30,
+            ],
+            'country'    => [
+                'title'        => __('Pays', 'tify'),
+                'type'         => 'text',
+                'required'     => true,
+                'attrs'        => [
+                    'autocomplete' => 'country',
+                ],
+                'order'        => 40,
+            ],
+            'address_1'  => [
+                'title'        => __('Numéro et nom de rue', 'tify'),
+                'type'         => 'text',
+                'required'     => true,
+                'attrs'        => [
+                    'autocomplete' => 'address-line1',
+                    'placeholder'  => esc_attr__('N° et rue', 'tify'),
+                ],
+                'order'        => 50,
+            ],
+            'address_2'  => [
+                'title'        => __('Complément d\'adresse', 'tify'),
+                'type'         => 'text',
+                'attrs'        => [
+                    'autocomplete' => 'address-line2',
+                    'placeholder'  => esc_attr__('Appartement, porte, bureau, etc. (optionnel)', 'tify'),
+                ],
+                'order'        => 60,
+            ],
+            'city'       => [
+                'title'        => __('Ville', 'tify'),
+                'type'         => 'text',
+                'required'     => true,
+                'attrs'        => [
+                    'autocomplete' => 'address-level2',
+                ],
+                'order'        => 70,
+            ],
+            'state'      => [
+                'title'        => __('Département / Région', 'tify'),
+                'type'         => 'text',
+                'required'     => true,
+                'attrs'        => [
+                    'autocomplete' => 'address-level1',
+                ],
+                'order'        => 80,
+            ],
+            'postcode'   => [
+                'title'        => __('Code postal', 'tify'),
+                'type'         => 'text',
+                'required'     => true,
+                'attrs'        => [
+                    'autocomplete' => 'postal-code',
+                ],
+                'order'        => 90,
+            ]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function shipping()
+    {
+        return app('shop.addresses.shipping', [$this, $this->shop]);
+    }
+}
---
+++ shop/tags/2.0.10/2.0/Addresses/Billing.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace tiFy\Plugins\Shop\Addresses;
+
+use tiFy\Plugins\Shop\Contracts\AddressBillingInterface;
+
+/**
+ * Class Billing
+ *
+ * @desc Gestion des adresses de facturation.
+ */
+class Billing extends AbstractAddress implements AddressBillingInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function fields()
+    {
+        $fields = parent::fields();
+
+        $fields['phone'] = [
+            'title'        => __('Numéro de téléphone', 'tify'),
+            'type'         => 'text',
+            'required'     => true,
+            'attrs'        => [
+                'autocomplete' => 'phone',
+            ],
+            'order'        => 100
+        ];
+
+        $fields['email'] = [
+            'title'        => __('Adresse de messagerie', 'tify'),
+            'type'         => 'text',
+            'required'     => true,
+            'attrs'        => [
+                'autocomplete' => 'email',
+            ],
+            'order'        => 110
+        ];
+
+        return $fields;
+    }
+}

c689b94b8849d6856fcade67f9200f8e99029cb6
gaetanthery <gaetanthery@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-04-04 08:26:12 +0000

    shop -- tag 2.0.26 -- Ajout d'un événement à l'issu du paiement

---
+++ shop/tags/2.0.26/Notices/Notices.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace tiFy\Plugins\Shop\Notices;
+
+use LogicException;
+use tiFy\Plugins\Shop\AbstractShopSingleton;
+use tiFy\Plugins\Shop\Contracts\NoticesInterface;
+
+/**
+ * Class Notices
+ *
+ * @desc Gestion des messages de notification.
+ */
+class Notices extends AbstractShopSingleton implements NoticesInterface
+{
+    /**
+     * Liste des messages de notification à afficher
+     * @var array
+     */
+    protected $notices = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        add_action('wp_loaded', function() {
+            $this->notices = $this->session()->get('notices', []);
+        });
+    }
+
+    /**
+     * Affichage des message de notification
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->display();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add($message, $type = 'success')
+    {
+        if (!did_action('wp_loaded')) :
+            throw new LogicException(
+                __(
+                    'L\'ajout de message de notification ne devrait pas être fait ' . '
+                    à ce moment de l\'exécution de votre code',
+                    'tify'
+                ),
+                500
+            );
+        endif;
+
+        if (!isset($this->notices[$type])) :
+            $this->notices[$type] = [];
+        endif;
+        $this->notices[$type][] = $message;
+
+        $this->session()->put('notices', $this->notices);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear()
+    {
+        $this->notices = [];
+        $this->session()->put('notices', []);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function display()
+    {
+        if (!did_action('template_redirect')) :
+            throw new \LogicException(
+                __(
+                    'L\'affichage des messages de notifications ne devrait pas être fait ' . '
+                    à ce moment de l\'execution de votre code',
+                    'tify'
+                ),
+                500
+            );
+        endif;
+
+        if (!$this->notices) :
+            return '';
+        endif;
+
+        $output = "";
+        foreach ($this->notices as $type => $messages) :
+            foreach($messages as $content) :
+                $output .= (string)partial('notice', compact('type', 'content'));
+            endforeach;
+        endforeach;
+
+        $this->clear();
+
+        return $output;
+    }
+}

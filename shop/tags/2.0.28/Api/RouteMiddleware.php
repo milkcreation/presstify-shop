6ddf539abdb491cd68471892156f9e7ddadc0801
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-04-11 09:30:15 +0000

    shop -- tag 2.0.28 -- Correctif bug Api

---
+++ shop/tags/2.0.28/Api/RouteMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace tiFy\Plugins\Shop\Api;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use tiFy\Contracts\Http\Request;
+use tiFy\Http\Response;
+
+class RouteMiddleware implements MiddlewareInterface
+{
+    /**
+     * Vérification de l'authentification.
+     *
+     * @param Request $request
+     *
+     * @return bool
+     */
+    protected function isAuth(Request $request)
+    {
+        return in_array($request->get('authtoken'), config('shop.api.authtoken', []));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(ServerRequestInterface $psrRequest, RequestHandlerInterface $handler) : ResponseInterface
+    {
+        $request = request()->createFromPsr($psrRequest);
+
+        if (!$this->isAuth($request)) {
+            $response = Response::convertToPsr();
+            $response->getBody()->write(json_encode(['error' => 'Accès restreint, clé d\'autorisation invalide']));
+            $response->withStatus(401);
+        } else {
+            $response = $handler->handle($psrRequest);
+        }
+        return $response;
+    }
+}

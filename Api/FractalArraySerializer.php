c3c5384ffdbc5897fb282c6af4a00e036be51856
Jordy Manner <jordy.manner@milkcreation.fr>
2019-04-12 16:32:16 +0000

    shop -- tag 2.0.29 -- Correctifs réécriture tiFy + injection de dépendances

---
+++ Api/FractalArraySerializer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace tiFy\Plugins\Shop\Api;
+
+use League\Fractal\Serializer\ArraySerializer;
+
+class FractalArraySerializer extends ArraySerializer
+{
+    /**
+     * @inheritdoc
+     */
+    public function collection($resourceKey, array $data)
+    {
+        if ($resourceKey) {
+            return [$resourceKey => $data];
+        }
+        return $data;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function item($resourceKey, array $data)
+    {
+        if ($resourceKey) {
+            return [$resourceKey => $data];
+        }
+        return $data;
+    }
+}
--- Orders/OrderItems/OrderItemList.php
+++ Orders/OrderItems/OrderItemList.php
@@ -1,46 +1,32 @@
 <?php
 
-/**
- * @name OrderItemList
- * @desc Controleur des éléments de commande en base de données.
- * @package presstiFy
- * @namespace \tiFy\Plugins\Shop\Orders\OrderItems
- * @version 1.1
- * @since 1.1
- *
- * @author Jordy Manner <jordy@tigreblanc.fr>
- * @copyright Milkcreation
- */
-
 namespace tiFy\Plugins\Shop\Orders\OrderItems;
 
 use illuminate\Support\Collection;
-use tiFy\Apps\AppTrait;
+use tiFy\Plugins\Shop\Contracts\OrderItemInterface;
+use tiFy\Plugins\Shop\Contracts\OrderItemListInterface;
 use tiFy\Plugins\Shop\Shop;
-use tiFy\Plugins\Shop\ServiceProvider\ProvideTraits;
-use tiFy\Plugins\Shop\ServiceProvider\ProvideTraitsInterface;
+use tiFy\Plugins\Shop\ShopResolverTrait;
 
-class OrderItemList extends Collection implements ProvideTraitsInterface, OrderItemListInterface
+/**
+ * Class OrderItemList
+ *
+ * @desc Controleur des éléments de commande en base de données.
+ */
+class OrderItemList extends Collection implements OrderItemListInterface
 {
-    use AppTrait, ProvideTraits;
-
-    /**
-     * Classe de rappel de la boutique.
-     * @var Shop
-     */
-    protected $shop;
+    use ShopResolverTrait;
 
     /**
      * CONSTRUCTEUR.
      *
-     * @param OrderItem[] $items Liste des éléments.
-     * @param Shop $shop Classe de rappel de la boutique.
+     * @param OrderItemInterface[] $items Liste des éléments.
+     * @param Shop $shop Instance de la boutique.
      *
      * @return void
      */
     public function __construct($items = [], Shop $shop)
     {
-        // Définition de la classe de rappel de la boutique.
         $this->shop = $shop;
 
         parent::__construct($items);

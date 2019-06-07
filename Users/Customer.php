7688ebe73ead0846b898bdd8212ef2c03ae0516d
Jordy Manner <jordy.manner@milkcreation.fr>
2019-06-07 17:29:00 +0000

    shop -- tag 2.0.33 -- Correctif Api Maj ZendDiactaros

--- Users/Customer.php
+++ Users/Customer.php
@@ -2,26 +2,16 @@
 
 namespace tiFy\Plugins\Shop\Users;
 
-use tiFy\Plugins\Shop\Orders\OrderListInterface;
-use tiFy\Plugins\Shop\Shop;
+use tiFy\Plugins\Shop\Contracts\OrderListInterface;
+use tiFy\Plugins\Shop\Contracts\UserCustomerInterface;
 
-class Customer extends AbstractUser implements CustomerInterface
+class Customer extends UserItem implements UserCustomerInterface
 {
-    /**
-     * Vérifie si un utilisateur est considéré en tant que client
-     *
-     * @return bool
-     */
-    public function isCustomer()
-    {
-        return true;
-    }
-
     /**
      * Récupération de la liste des commandes du client
      *
      * @param array $query_args Liste des arguments de requête personnalisée.
-     * 
+     *
      * @return OrderListInterface
      */
     public function getOrderList($query_args = [])
@@ -39,7 +29,15 @@ class Customer extends AbstractUser implements CustomerInterface
                 'value' => $this->getId()
             ]
         ];
-        
-        return $this->orders()->getList($query_args);
+
+        return $this->orders()->getCollection($query_args);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isCustomer()
+    {
+        return true;
     }
 }

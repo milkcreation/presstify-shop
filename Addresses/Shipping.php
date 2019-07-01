b88bbae0fb9127d289b784f89feeaf113d735a57
Jordy Manner <jordy.manner@milkcreation.fr>
2019-07-01 05:35:07 +0000

    shop -- tag 2.0.36 -- Shop.php surchargeable

--- Addresses/Shipping.php
+++ Addresses/Shipping.php
@@ -1,20 +1,15 @@
 <?php
 
-/**
- * @name Shipping
- * @desc Gestion des adresses de livraison
- * @package presstiFy
- * @namespace \tiFy\Plugins\Shop\Address
- * @version 1.1
- * @since 1.2.600
- *
- * @author Jordy Manner <jordy@tigreblanc.fr>
- * @copyright Milkcreation
- */
-
 namespace tiFy\Plugins\Shop\Addresses;
 
-class Shipping extends AbstractAddress implements ShippingInterface
+use tiFy\Plugins\Shop\Contracts\AddressShippingInterface;
+
+/**
+ * Class Shipping
+ *
+ * @desc Gestion des adresses de livraison.
+ */
+class Shipping extends AbstractAddress implements AddressShippingInterface
 {
 
 }

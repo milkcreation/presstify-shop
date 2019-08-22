f92d50c6dc18d520cb25b19c0f2401ee4d9aef5a
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-08-22 07:36:40 +0000

    shop -- tag 2.0.37 -- Correctif dépendances

---
+++ shop/tags/2.0.37/Contracts/Actions.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace tiFy\Plugins\Shop\Contracts;
+
+interface Actions extends BootableControllerInterface, ShopResolverInterface
+{
+    /**
+     * Récupération de l'url d'une action de traitement.
+     *
+     * @param string $alias Alias de qualification de l'action.
+     * @param array $parameters Liste des variables passées en argument dans l'url.
+     * @param boolean $absolute Format de sortie de l'url. Url relative par défaut.
+     *
+     * @return string
+     */
+    public function url($alias, $parameters = [], $absolute = false);
+}

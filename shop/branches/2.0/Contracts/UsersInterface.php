7124346e8c9a47b6e1b954522745f4e3ee1963d6
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-08-28 06:19:16 +0000

    shop -- branch 2.0 -- User accessibilité de l'utilisateur courant ou selon son ID

--- shop/branches/2.0/Contracts/UsersInterface.php
+++ shop/branches/2.0/Contracts/UsersInterface.php
@@ -4,20 +4,21 @@ namespace tiFy\Plugins\Shop\Contracts;
 
 use tiFy\Contracts\User\UserQuery;
 use tiFy\Contracts\User\SigninFactory;
+use WP_User;
 
 interface UsersInterface extends BootableControllerInterface, ShopResolverInterface, UserQuery
 {
     /**
      * Récupération de l'object utilisateur courant de Wordpress.
      *
-     * @return null|\WP_User
+     * @return WP_User|null
      */
     public function current();
 
     /**
      * Récupération des données d'un utilisateur
      *
-     * @param string|int|\WP_User|null $user Identifiant pixvert|ID de l'utilisateur WP|Objet User WP
+     * @param string|int|WP_User|null $user Identifiant pixvert|ID de l'utilisateur WP|Objet User WP
      * |Utilisateur courant WP.
      *
      * @return object|UserCustomerInterface|UserLoggedOutInterface|UserShopManagerInterface|UserItemInterface.

e8ce25df2a3ba54d7ca65a1544d0d1ed9bd77373
Jordy Manner <jordy.manner@milkcreation.fr>
2019-08-31 08:44:10 +0000

    shop -- branch 2.0 -- Gestion route mutlisite

--- Routing/Routes.php
+++ Routing/Routes.php
@@ -21,15 +21,8 @@ class Routes implements RoutesContract
      */
     public function boot(): void
     {
-        $prefix = '/';
-        if (is_multisite()) {
-            $prefix = get_blog_details()->path !== '/'
-                ? rtrim(preg_replace('#^' . url()->rewriteBase() . '#', '', get_blog_details()->path), '/') . '/'
-                : '/';
-        }
-
         // API
-        Router::group("{$prefix}shop/api", function (RouteGroup $router) {
+        Router::group('shop/api', function (RouteGroup $router) {
             // Racine - Documentation
             $router->get('/', [$this->shop->resolve('api'), 'rootEndpoint']);
 
@@ -42,26 +35,26 @@ class Routes implements RoutesContract
 
         // PANIER
         // Ajout d'un article au panier
-        Router::post("{$prefix}ajouter-au-panier/{product_name}", [$this->shop->cart(), 'addHandler'])
+        Router::post('ajouter-au-panier/{product_name}', [$this->shop->cart(), 'addHandler'])
             ->setName('shop.cart.add');
 
         // Mise à jour des articles du panier
-        Router::post("{$prefix}mise-a-jour-du-panier", [$this->shop->cart(), 'updateHandler'])
+        Router::post('mise-a-jour-du-panier', [$this->shop->cart(), 'updateHandler'])
             ->setName('shop.cart.update');
 
         // Suppression d'un article du panier
-        Router::get("{$prefix}supprimer-du-panier/{line_key}", [$this->shop->cart(), 'removeHandler'])
+        Router::get('supprimer-du-panier/{line_key}', [$this->shop->cart(), 'removeHandler'])
             ->setName('shop.cart.remove');
 
         // PAIEMENT
         // Traitement de la commande
-        Router::post("{$prefix}shop/checkout/process", [$this->shop->checkout(), 'process'])
+        Router::post('shop/checkout/process', [$this->shop->checkout(), 'process'])
             ->setName('shop.checkout.process');
 
         // COMMANDE
         // Validation de paiement
         Router::post(
-            "{$prefix}shop/order/payment_complete/{order_id:number}",
+            'shop/order/payment_complete/{order_id:number}',
             [$this->shop->orders(), 'handlePaymentComplete']
         )->setName('shop.order.payment_complete');
     }
